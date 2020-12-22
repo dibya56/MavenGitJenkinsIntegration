a30bc9d83a64c4974f403ad90e9a9c477f3f9950
Dibya Saha <dibya.ahas1996@gmail.com>
2020-12-22 19:54:28 +0530

    Log Message

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import java.lang.System.Logger;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,6 +25,7 @@ public class SearchTest {
 		try {
 			Thread.sleep(3000);
 			driver.findElement(By.xpath("//*[@id=\"TRAVEL_TECHNOLOGY_PARTNER\"]")).click();
+			System.out.println("AA");
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
